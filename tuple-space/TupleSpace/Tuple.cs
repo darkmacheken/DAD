bfc3f68f4aa6f5c50b2381ce7e7648ddca3896c2
mafsbaptista <mafa.ferreira96@gmail.com>
2018-11-05 22:18:44 +0000

    #2 Basic Structure of Tuple Space.

---
+++ tuple-space/TupleSpace/Tuple.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TupleSpace {
+
+    public class Tuple {
+        // A tuple consists in a list of fields.
+        public List<IField> fields = new List<IField>();
+
+        public static Tuple ParseTuple(string tuple) {
+            return null;
+        }
+    }
+
+    // Each field has 2 properties: the type of the field and its value.
+    public interface IField {
+        Type Type { get; set; }
+        object Value { get; set; }
+    }
+
+    public class Field<DataType> : IField {
+
+        public Field(DataType value) {
+            Value = value;
+            Type = value.GetType();
+        }
+
+        public Type Type { get; set; }
+
+        public object Value { get; set; }
+    }
+}
---
+++ tuple-space/TupleSpace/TupleSpace.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace TupleSpace {
+
+    public class TupleSpace {
+        List<Tuple> tuples = new List<Tuple>();
+
+        public void Add(string tuplestring) {
+            Tuple tuple = Tuple.ParseTuple(tuplestring);
+        }
+
+        public Tuple Read(string tuplestring) {
+            Tuple tuple = Tuple.ParseTuple(tuplestring);
+            return tuple;
+        }
+
+        public Tuple Take(string tuplestring) {
+            Tuple tuple = Tuple.ParseTuple(tuplestring);
+            return tuple;
+        }
+
+    }
+
+
+}
+
+
