b15f60472200265b1cd79bafa85b4ae18a9fb4e7
mafsbaptista <mafa.ferreira96@gmail.com>
2018-11-06 16:57:23 +0000

    #2 Add TestClasses file and Add correct parsing of string tuple.

--- tuple-space/TupleSpace/Tuple.cs
+++ tuple-space/TupleSpace/Tuple.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 
@@ -12,33 +13,51 @@ namespace TupleSpace {
         public Tuple(string tuple) {
             Fields = new List<IField>();
             ParseInput(tuple);
-
         }
 
         void ParseInput(string tuple) {
-            List<string> matches = new List<string>();
+
+            List<string> fields = new List<string>(); //stores the parameters of the tuple.
             string pattern = @"(\"".*?\"")+|((\d)+)|(\w)*?(\(.*?\))|(\w)+";
-
             Regex rgx = new Regex(pattern);
+
             foreach (Match match in rgx.Matches(tuple))
-                matches.Add(match.Value);
+                fields.Add(match.Value);
 
-            foreach (var match in matches) {
-
-                //if string
-                if (match.StartsWith("\"", StringComparison.Ordinal)) {
-                    Field<string> field = new Field<string>(match.Substring(1, match.Length - 2)); //remove the quotes
-                    this.Fields.Add(field);
+            foreach (var field in fields) {
+                /* If the field is a string */
+                if (field.StartsWith("\"", StringComparison.Ordinal)) {
+                    /* Add to Tuple fields*/
+                    Field<string> new_field = new Field<string>(field.Substring(1, field.Length - 2)); //remove the quotes
+                    this.Fields.Add(new_field);
                 }
 
-                //if object
-                // objects are represented by a string array like ["nameofobject/class", [args]]
+                /* If the field is an object */
                 else {
                     char[] charSeparators = { ',', '(', ')' };
-                    string[] res = match.Split(charSeparators);
-                    Field<object> field = new Field<object>(res);
-                    this.Fields.Add(field);
-                 }
+                    string[] res = field.Split(charSeparators);
+
+                    /* If it's a name - className or null */
+                    if (res.Length == 1) {
+                        /* Add to Tuple fields*/
+                        Field<string> new_field = new Field<string>(field);
+                        this.Fields.Add(new_field);
+                    }
+                    /* If it's a constructor */
+                    else {
+                        string className = res[0];
+                        List<string> args = res.ToList().GetRange(1, res.Length - 2); //removes the className and the last match (empty)
+                        List<object> parsed_args = ParseArgs(args);
+
+                        /* Instanciate object from the given constructor */
+                        Type t = Type.GetType(String.Concat("TupleSpace.", className));
+                        object obj = Activator.CreateInstance(t, parsed_args.ToArray());
+
+                        /* Add to Tuple fields*/
+                        Field<object> new_field = new Field<object>(obj);
+                        this.Fields.Add(new_field);
+                    }
+                }
             }
         }
 
@@ -80,12 +99,40 @@ namespace TupleSpace {
             }
             return true;
         }
+
+        public override string ToString() {
+            string fields = "";
+            foreach (IField field in Fields) {
+                String.Concat(fields, field.Value, " ");
+            }
+            return "Tuple: " + fields;
+        }
+
+        List<object> ParseArgs(List<string> args) {
+            List<object> parsed_args = new List<object>();
+
+            foreach (string arg in args) {
+                if (arg.Length <= 0) { //empty match
+                    continue;
+                }
+                if (arg.Contains("\"")) { //if arg is a string
+                    string parsed_arg = Regex.Replace(arg, @"(\s|\"")*", "");
+                    parsed_args.Add(parsed_arg);
+                }
+                else { //if arg is an int
+                    int parsed_arg = Int32.Parse(arg);
+                    parsed_args.Add(parsed_arg);
+                }
+            }
+            return parsed_args;
+        }
     }
 
     // Each field has 2 properties: the type of the field and its value.
     public interface IField {
         Type Type { get; set; }
         object Value { get; set; }
+        string ToString();
     }
 
     public class Field<DataType> : IField {
@@ -98,5 +145,10 @@ namespace TupleSpace {
         public Type Type { get; set; }
 
         public object Value { get; set; }
+
+        public override string ToString()
+        {
+            return Type + "(" + Value + ")";
+        }
     }
 }
