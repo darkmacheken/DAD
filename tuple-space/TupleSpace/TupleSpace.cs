90e84d4dfd2390c192ff349c93e713730466b3d9
mafsbaptista <mafa.ferreira96@gmail.com>
2018-11-07 22:07:17 +0000

    #2 Attempt of implementation of LX operations with locks.

--- tuple-space/TupleSpace/TupleSpace.cs
+++ tuple-space/TupleSpace/TupleSpace.cs
@@ -1,51 +1,45 @@
 ﻿using System.Collections.Generic;
 using System;
+using System.Threading;
 
 namespace TupleSpace {
 
-    public class TupleSpace
+    public class TupleSpace 
     {
         public List<Tuple> Tuples { get; set; }
 
-        public TupleSpace()
-        {
+        public TupleSpace() {
             Tuples = new List<Tuple>();
         }
 
-        public TupleSpace(List<Tuple> tuples)
-        {
+        public TupleSpace(List<Tuple> tuples) {
             Tuples = new List<Tuple>(tuples);
         }
 
-        public void Add(string tuplestring)
-        {
+        public void Add(string tuplestring) {
             Tuple tuple = new Tuple(tuplestring);
-            Tuples.Add(tuple);
+            this.Tuples.Add(tuple);
             Console.WriteLine("Added Tuple: " + tuple);
         }
 
-        public Tuple Read(string tuplestring)
-        {
+        public Tuple Read(string tuplestring) {
             Tuple tuple = new Tuple(tuplestring);
             Console.WriteLine("\nRead Tuple: " + tuple);
             List<Tuple> matches = SearchTuples(tuple);
             Console.WriteLine("Matches: -------------------------- ");
-            foreach (Tuple match in matches)
-            {
+            foreach (Tuple match in matches) {
                 Console.WriteLine(match);
             }
             Console.WriteLine("-----------------------------------");
             return matches[0];
         }
 
-        public Tuple Take(string tuplestring)
-        {
+        public Tuple Take(string tuplestring) {
             Tuple tuple = new Tuple(tuplestring);
             Console.WriteLine("\nTake Tuple: " + tuple);
             List<Tuple> matches = SearchTuples(tuple);
             Console.WriteLine("Matches: -------------------------- ");
-            foreach (Tuple match in matches)
-            {
+            foreach (Tuple match in matches) {
                 Console.WriteLine(match);
             }
             Console.WriteLine("-----------------------------------");
@@ -54,22 +48,68 @@ namespace TupleSpace {
             return removed;
         }
 
-        List<Tuple> SearchTuples(Tuple search_tuple)
-        {
-            //lock tuple list in order to don't add or take any tuple that might compromise the result.
-            //lock(tuples)
+        public List<Tuple> GetAndLock(int clientid, int requestnumber, string tuplestring) {
+            Tuple search_tuple = new Tuple(tuplestring);
+            List<Tuple> matches = new List<Tuple>();
+            List<Tuple> locked_tuples = new List<Tuple>();
+            bool refuse = false;
+            /* only one thread can aquire locks at each time */
+            lock (this.Tuples) {
+                matches = SearchTuples(search_tuple);
+                /* lock all matches */
+                foreach (Tuple tuple in matches) {
+                    /* if lock cannot be acquired */
+                    if(!Monitor.TryEnter(tuple)) {
+                        refuse = true;
+                        break;
+                    }
+                    locked_tuples.Add(tuple);
+                }
+                /* unlock previous locked tuples */
+                if(refuse) {
+                    foreach(Tuple tuple in locked_tuples) {
+                        Monitor.Exit(tuple);
+                    }
+                    return null;
+                }
+                else {
+                    //add to tuple set with clientid and requestnumber
+                }
+            }
+            return matches;
+        }
+
+        public void Unlock(int clientid, int requestnumber) {
+            List<Tuple> locked_tuples = GetTuplesByClientRequest(clientid, requestnumber);
+            /* lock all matches */
+            foreach (Tuple tuple in locked_tuples) {
+                Monitor.Exit(tuple);
+            }
+        }
+
+        public void UnlockAndTake(int clientid, int requestnumber, Tuple tupletoremove) {
+            List<Tuple> locked_tuples = GetTuplesByClientRequest(clientid, requestnumber);
+
+            /* unlock and take are atomic */
+            lock (this.Tuples) {
+                Unlock(clientid, requestnumber);
+                this.Tuples.Remove(tupletoremove);
+            }
+        }
+
+        private List<Tuple> GetTuplesByClientRequest(int clientid, int requestnumber) {
+            //todo implementation of list? of lists? tuples
+            return null;
+        }
+
+        List<Tuple> SearchTuples(Tuple search_tuple) {
             List<Tuple> result = new List<Tuple>();
-            foreach (Tuple tuple in Tuples)
-            {
-                if (tuple.Match(search_tuple))
-                {
+            foreach (Tuple tuple in this.Tuples) {
+                if (tuple.Match(search_tuple)) {
                     result.Add(tuple);
                 }
             }
-            //unlock(tuples)
             return result;
         }
     }
 }
-
-
