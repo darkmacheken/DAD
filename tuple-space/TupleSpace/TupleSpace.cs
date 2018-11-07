d6f57169a1a36c98dd02c96cd35d366add2ec076
mafsbaptista <mafa.ferreira96@gmail.com>
2018-11-07 17:16:38 +0000

    #2 Add operations Add, Read, Take without lock/blocking semantics.

--- tuple-space/TupleSpace/TupleSpace.cs
+++ tuple-space/TupleSpace/TupleSpace.cs
@@ -1,31 +1,68 @@
 ﻿using System.Collections.Generic;
+using System;
 
 namespace TupleSpace {
 
-    public class TupleSpace {
-        List<Tuple> tuples = new List<Tuple>();
+    public class TupleSpace
+    {
+        public List<Tuple> Tuples { get; set; }
 
-        public void Add(string tuplestring) {
-            Tuple tuple = new Tuple(tuplestring);
-            tuples.Add(tuple);
+        public TupleSpace()
+        {
+            Tuples = new List<Tuple>();
         }
 
-        public Tuple Read(string tuplestring) {
-            Tuple tuple = new Tuple(tuplestring);
-            return null;
+        public TupleSpace(List<Tuple> tuples)
+        {
+            Tuples = new List<Tuple>(tuples);
         }
 
-        public Tuple Take(string tuplestring) {
+        public void Add(string tuplestring)
+        {
             Tuple tuple = new Tuple(tuplestring);
-            return null;
+            Tuples.Add(tuple);
+            Console.WriteLine("Added Tuple: " + tuple);
         }
 
-        private List<Tuple> SearchTuple(Tuple search_tuple) {
+        public Tuple Read(string tuplestring)
+        {
+            Tuple tuple = new Tuple(tuplestring);
+            Console.WriteLine("\nRead Tuple: " + tuple);
+            List<Tuple> matches = SearchTuples(tuple);
+            Console.WriteLine("Matches: -------------------------- ");
+            foreach (Tuple match in matches)
+            {
+                Console.WriteLine(match);
+            }
+            Console.WriteLine("-----------------------------------");
+            return matches[0];
+        }
+
+        public Tuple Take(string tuplestring)
+        {
+            Tuple tuple = new Tuple(tuplestring);
+            Console.WriteLine("\nTake Tuple: " + tuple);
+            List<Tuple> matches = SearchTuples(tuple);
+            Console.WriteLine("Matches: -------------------------- ");
+            foreach (Tuple match in matches)
+            {
+                Console.WriteLine(match);
+            }
+            Console.WriteLine("-----------------------------------");
+            Tuple removed = matches[0];
+            Tuples.Remove(removed);
+            return removed;
+        }
+
+        List<Tuple> SearchTuples(Tuple search_tuple)
+        {
             //lock tuple list in order to don't add or take any tuple that might compromise the result.
             //lock(tuples)
             List<Tuple> result = new List<Tuple>();
-            foreach(Tuple tuple in tuples) {
-                if (tuple.Match(search_tuple)) {
+            foreach (Tuple tuple in Tuples)
+            {
+                if (tuple.Match(search_tuple))
+                {
                     result.Add(tuple);
                 }
             }
